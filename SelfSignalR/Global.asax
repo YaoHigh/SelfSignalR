3651d373a92f46568c9e88f5e0a935416196992c
High <15385150561@163.com>
2018-01-16 12:00:07 +0800

    第一次提交

---
+++ SelfSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SelfSignalR.MvcApplication" Language="C#" %>
---
+++ SelfSignalR2.0/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SelfSignalR2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chat(string name)
+        {
+            Session["userid"] = Guid.NewGuid().ToString().ToUpper();
+            Session["username"] = name;
+            return View();
+        }
+    }
+}
